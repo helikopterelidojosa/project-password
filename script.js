96b11d95151b6957f440098193622373268b712e
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2022-01-10 19:14:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let parole = '007';
+let inputPW;
+
+function givePW(){
+ inputPW = document.getElementById('pw').value;
+
+    if(parole == inputPW){
+        document.getElementById('new').style.display = 'block';
+
+        return inputPW;
+    }
+    else
+    {
+        alert('the password is incorrect!!!');
+    };
+};
+
+function fun1(){
+
+
+
+
+    if (parole == inputPW){
+
+        
+
+
+        let chTXT = document.getElementById('inp').value;
+
+
+
+        document.getElementById('para').innerHTML = 'good! your text goes like this: '+ chTXT;
+
+
+        
+    }
+    
+};
+
